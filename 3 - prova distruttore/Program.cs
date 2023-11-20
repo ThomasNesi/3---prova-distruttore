fa61e3ed9bb0e42875dce6d2f7dc42b68ec0205e
NESI.20118 <NESI.20118@LABI2-13PC21>
2023-11-20 09:47:03 +0100

    Aggiungere i file di progetto.

---
+++ 3 - prova distruttore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3___prova_distruttore
+{
+    class Esempio
+    {
+        private string stringa;
+        private int numero;
+
+        public Esempio(string stringa, int numero)
+        {
+            this.stringa = stringa;
+            this.numero = numero;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
