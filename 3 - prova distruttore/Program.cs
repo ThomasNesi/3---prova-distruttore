5e6e7986dbd489b774db4c554657605c86f721ae
ThomasNesi <thomasnesi9@gmail.com>
2023-11-21 10:45:27 +0100

    mod

--- 3 - prova distruttore/Program.cs
+++ 3 - prova distruttore/Program.cs
@@ -6,21 +6,60 @@ using System.Threading.Tasks;
 
 namespace _3___prova_distruttore
 {
-    class Esempio
+    class Esempio : IDisposable
     {
+        //attributi
         private string stringa;
         private int numero;
 
+        public Esempio()
+        {
+            stringa = " ";
+            numero = 0;
+        }
+
+        // Costruttori con parametri
         public Esempio(string stringa, int numero)
         {
             this.stringa = stringa;
             this.numero = numero;
         }
+
+        //Metodi accessor
+        public string Stringa()
+        {
+            return stringa;
+        }
+
+        public int Numero()
+        {
+            return numero;
+        }
+
+        //distuttore
+        ~Esempio()
+        {
+            Console.WriteLine("Distruttore chiamato. Pulizia delle risorse non gestite.");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose chiamato. Pulizia delle risorse gestite.");
+            GC.SuppressFinalize(this);
+        }
     }
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Creazione dell'istanza di Esempio.");
+            using (Esempio es = new Esempio("Hello", 42))
+            {
+                Console.WriteLine($"Stringa: {es.Stringa()}, Numero: {es.Numero()}");
+            }
+
+            Console.WriteLine("Fine del Main. L'oggetto Esempio è stato distrutto o eliminato.");
+            Console.ReadLine();
         }
     }
 }
